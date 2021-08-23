4136a4e7ebd43cf57194abea721b0c5ec928e05d
erainterfacesindia <erainterfacesindia>
2021-08-24 00:12:11 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,50 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title:'Era Interface',
+    description :'Era Interfaces website',
+    copyright : 'This website is copyright 2021 Era Interfaces pvt ltd.',
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `video`, 
+        path: `${__dirname}/src/assets/videos`,
+      },
+    },
+      
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/assets/images`,
+        },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+  ],
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,16 @@
-import { Link } from '@reach/router'
-import React from 'react'
-import Layout from '../component/layout'
-import * as styles from '../styles/home.module.css'
-
-export default function Home() {
-    return (
-        <Layout>
-        <section className={styles.header}>  
-            <div >
-                <h2 > Our technology enabled solutions are tailored to meet the demands of individuals & organisations</h2>
-                <Link className={styles.btn} to="/about">Explore More!</Link>         
-        </div>
-        < img src="/eragif.gif" alt="home page gif" style={{maxWidth: '100%', maxHeight:"100%"}}/>
-        </section>
-        <section >
-          <div>
-            <h1> Our Services</h1>
-
-          </div>
-          < img src="/gatsby.png" alt="home page " style={{maxWidth: '25%', maxHeight:"50%"}}/>
-        </section>
-       
-        </Layout>
-    )
-}
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import Hero from "../components/HeroSection"
 
 
 
+const IndexPage = () => (
+  <Layout>
+    <Hero/>
+    {/* <Ourservices heading="Our Services"/>  */}
+    {/* <OurservicesSection /> */}
+  </Layout>
+)
+
+export default IndexPage
