b0153039a177b51fcba0c9bbb40bfba7a1b8c172
erainterfacesindia <erainterfaces@gmail.com>
2022-01-20 18:30:32 +0530

    commit

--- src/components/Pages/Jamstack/Hero.js
+++ src/components/Pages/Jamstack/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
             <div class="text-container text-container--medium">
                 <div class="hero_content grid items-center">
                     <div class="hero_body my-2 cell-12 text-center">
-                    <h1 class="hero_title">What is JAM STACK?</h1>
+                    <h1 class="hero_title">What is JAMstack?</h1>
                     <div class="hero_paragraph">
                 <p class="text-center">JAM is the acronym for Javascript, API, and Markup language. It is an architecture designed to make a web, just as iron rods are to a building. As the nomenclature says it is a combination of Javascript- a programming language, API- acronym for Application Programming Interface, 
                     and Markup language to make an application. JAMStack is changing the dynamics of modern web architecture by following ways:</p>
